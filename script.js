0154a98457ebfd65c9defb36e229924b04cb9c71
Robert Leung <rleung84@uwo.ca>
2023-08-07 18:48:58 -0400

    updates to gameboard display

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 // use modules for one of something, use factories for multiple of something
 const gameboard = (() => {
-  arr = ["x", "x", "x", "x", "x", "x", "x", "x", "x"];
+  let arr = Array(9).fill("");
   return { arr };
 })();
 const displayController = (() => {
   const render = (arr) => {
     let content = document.querySelector(".content");
+    while (content.firstChild) {
+      content.removeChild(content.firstChild);
+    }
     for (let i = 0; i < arr.length; i++) {
       let div = document.createElement("div");
-      div.classList.add("gameboard");
+      div.classList.add("box");
       div.innerText = arr[i];
       content.appendChild(div);
     }
@@ -16,6 +19,9 @@ const displayController = (() => {
   return { render };
 })();
 const player = (name) => {
-  this.name = name;
   return { name };
 };
+
+displayController.render(gameboard.arr);
+gameboard.arr[4] = "X";
+displayController.render(gameboard.arr);
